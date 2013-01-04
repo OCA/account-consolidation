df3dacaf7278e7ea7138d91f4ef55924893896f2
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-01-04 11:06:11 +0100

    [FIX] licensed under AGPLv3 instead of GPL2
    [MIGR] set modules not installable (need migration)
    
    [IMP] manifest
    
    [FIX] xml indentation
    
    [FIX] no longer view_id in account.journal
    
    [IMP] python formatting and openerp standards
    
    [MIGR] migration of views
    
    [FIX] years were hardcoded in tests
    
    [FIX] february last day may change
    
    [FIX] test fiscal years conflicts with demo datas, the misconfigured fy have to be the next year
    
    [FIX] xmlid has changed
    
    [FIX] unclear docstring
    
    [FIX] currency rate type should return an id instead of a browse record, fix the docstring as well

--- account_consolidation_oca/account.py
+++ account_consolidation_oca/account.py
@@ -1,77 +1,71 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2011 Camptocamp SA (http://www.camptocamp.com)
+#    Author: Guewen Baconnier
+#    Copyright 2011-2013 Camptocamp SA
 #
-# Author : Guewen Baconnier (Camptocamp)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import osv, fields
+from openerp.osv import orm, fields
 
 
-class account_account(osv.osv):
+class account_account(orm.Model):
     _inherit = 'account.account'
 
     _columns = {
-        'consolidation_rate_type_id': fields.many2one('res.currency.rate.type',
-                                                      'Consolidation Currency Rate Type',
-                                                      help="Currency rate type used on this account for the consolidation, Leave empty to use the rate type of the account type."),
-        'consolidation_mode':  fields.selection([('', ''),
-                                                 ('ytd', 'YTD'),
-                                                 ('period', 'Period Movements'),
-                                                 ],
-                                                 'Consolidation Mode'),
+        'consolidation_rate_type_id': fields.many2one(
+            'res.currency.rate.type',
+            'Consolidation Currency Rate Type',
+            help="Currency rate type used on this account "
+                 "for the consolidation. "
+                 "Leave empty to use the rate type of the account type."),
+        'consolidation_mode': fields.selection(
+            [('ytd', 'YTD'),
+             ('period', 'Period Movements')],
+            'Consolidation Mode'),
     }
 
-account_account()
 
-
-class account_account_type(osv.osv):
+class account_account_type(orm.Model):
     _inherit = 'account.account.type'
 
     _columns = {
-        'consolidation_rate_type_id': fields.many2one('res.currency.rate.type',
-                                                      'Consolidation Currency Rate Type',
-                                                      help="Currency rate type used on this account type for the consolidation, Leave empty to use the 'spot' rate type."),
-        'consolidation_mode':  fields.selection([('ytd', 'YTD'),
-                                                 ('period', 'Period Movements'),],
-                                                 'Consolidation Mode'),
+        'consolidation_rate_type_id': fields.many2one(
+            'res.currency.rate.type',
+            'Consolidation Currency Rate Type',
+            help="Currency rate type used on this account type "
+                 "for the consolidation. "
+                 "Leave empty to use the 'spot' rate type."),
+        'consolidation_mode': fields.selection(
+            [('ytd', 'YTD'),
+             ('period', 'Period Movements')],
+            'Consolidation Mode'),
     }
 
     _defaults = {
         'consolidation_mode': 'ytd',
     }
 
-account_account_type()
 
-
-class account_move(osv.osv):
+class account_move(orm.Model):
     _inherit = 'account.move'
 
     _columns = {
-        'consol_company_id': fields.many2one('res.company', 'Consolidated from Company', readonly=True),
+        'consol_company_id': fields.many2one(
+            'res.company',
+            'Consolidated from Company',
+            readonly=True),
     }
-
-account_move()
--- account_consolidation_oca/company.py
+++ account_consolidation_oca/company.py
@@ -1,44 +1,33 @@
-# -*- coding: utf-8 -*-
+# -* coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2011 Camptocamp SA (http://www.camptocamp.com)
-# All Right Reserved
+#    Author: Guewen Baconnier
+#    Copyright 2011-2013 Camptocamp SA
 #
-# Author : Guewen Baconnier (Camptocamp)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import osv, fields
+from openerp.osv import orm, fields
 
 
-class res_company(osv.osv):
+class res_company(orm.Model):
     _inherit = 'res.company'
 
     _columns = {
-        'consolidation_chart_account_id': fields.many2one('account.account',
-                                            'Chart of Accounts for Consolidation',
-                                            domain=[('parent_id', '=', False)]),
+        'consolidation_chart_account_id': fields.many2one(
+            'account.account',
+            'Chart of Accounts for Consolidation',
+            domain=[('parent_id', '=', False)]),
     }
-
-res_company()
