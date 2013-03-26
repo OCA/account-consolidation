cd90c3adbbde5d8edd4a0f3e632ee3a2b08de115
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-03-26 09:50:59 +0100

    [FIX] using _logger.warning

--- account_parallel_currency/wizard/do_mapping.py
+++ account_parallel_currency/wizard/do_mapping.py
@@ -22,6 +22,9 @@
 
 from openerp.osv import fields, orm
 from tools.translate import _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class account_parallel_mapping(orm.TransientModel):
 
@@ -55,7 +58,7 @@ class account_parallel_mapping(orm.TransientModel):
                             raise orm.except_orm(_('Error'), _('Duplicated account %s for company %s')
                                 % (master_account.code,parallel_company.name))
                         elif not parallel_account_ids:
-                            print _('No account %s for company %s') % (master_account.code,parallel_company.name)
+                            _logger.warning(_('No account %s for company %s'), (master_account.code,parallel_company.name)) 
                         elif len(parallel_account_ids) == 1:
                             master_account.write({'parallel_account_ids':
                                 [(4,parallel_account_ids[0])]})
