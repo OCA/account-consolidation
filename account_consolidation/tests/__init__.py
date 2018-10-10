82277f99e382a7964597ad72883e3bd7f70523ac
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-10-10 11:52:30 +0200

    Import tests

--- account_consolidation/tests/__init__.py
+++ account_consolidation/tests/__init__.py
@@ -1,2 +1,7 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import test_account_company_rules
+from . import test_account_consolidation
+from . import test_analytic_multicompany
+from . import test_consolidation_profile
+from . import test_partner_multicompany
